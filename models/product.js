a082a1fa01ead1b4bae9c6b49c82f4610a41028e
Kuldeep Sahrma <kldpsh77@gmail.com>
2023-09-08 21:09:31 +0530

    models added

---
+++ models/product.js
@@ -0,0 +1,29 @@
+let products=[];
+const fs = require('fs')
+const path = require('path')
+const p = path.join(path.dirname(process.mainModule.filename),'data','products.json');
+
+const getProductsFromFile = (cb)=>{
+    fs.readFile(p,(err,data)=>{
+        if(err){
+            cb([])
+        }
+        cb(JSON.parse(data))
+    })
+}
+
+module.exports = class product{
+    constructor(t){
+        this.title = t
+    }
+    save(){
+        products=getProductsFromFile(products=>{
+            products.push(this)
+            fs.writeFile(p,JSON.stringify(products),err=>console.log(err))
+        })
+        
+    }
+    static fetchAll(cb){
+        getProductsFromFile(cb)
+    }
+}
